cca854e426910de4898ca76ce6ababe7284c578b
Ritik kumar <ritikkumarkashyap9@gmail.com>
2026-01-16 14:38:28 +0530

    ✅ 281A - Word-Capitalization

--- src/main/java/org/example/WordCapitalization.java
+++ src/main/java/org/example/WordCapitalization.java
@@ -6,7 +6,6 @@ public class WordCapitalization {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String word = sc.next();
-
         System.out.println(wordCapitalization(word));
     }
 
